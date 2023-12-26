83a7ad827503e64857408562c19f5d364ca2d252
Kiel <kiel02505@gmail.com>
2023-12-25 23:46:26 -0300

    Register Layout

--- app/src/main/java/com/kiel/deliveryapp/SinginActivity.java
+++ app/src/main/java/com/kiel/deliveryapp/SinginActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.kiel.deliveryapp.databinding.ActivitySinginBinding;
+
 public class SinginActivity extends AppCompatActivity {
+
+    private ActivitySinginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_singin);
+        binding = ActivitySinginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
